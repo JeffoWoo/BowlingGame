3d5ffc1a962d7d6922be7c77c9c7847457b5ffc6
JeffoWoo <webdevjeff@msn.com>
2018-09-13 17:42:52 +0100

    Final TDD solution to problem

--- BowlingGame.Tests/BowlingGameRollingAndScoringTests.cs
+++ BowlingGame.Tests/BowlingGameRollingAndScoringTests.cs
@@ -1,35 +1,111 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace BowlingGame.Tests
 {
     [TestFixture]
     public class BowlingGameRollingAndScoringTests
     {
+        #region Private Variables
+
+        private Game _game;
+
+        #endregion
+
+        #region Fixture Setup / Tear Down
+
+        [SetUp]
+        public void Setup()
+        {
+            _game = new Game();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _game = null;
+        }
+
+        #endregion
+
+        #region Fixture Test Methods
+
         [Test]
         public void Test_game_player_scores_all_gutter_balls()
         {
-            var game = new Game();
+            RollByMultipleAndPinsDown(20, 0);
 
-            for (int i = 0; i < 20; i++)
-            {
-                game.Roll(0);
-            }
-
-            Assert.AreEqual(0, game.Score());
+            Assert.AreEqual(0, _game.Score());
 
         }
 
         [Test]
         public void Test_game_player_scores_all_ones()
         {
-            var game = new Game();
+            RollByMultipleAndPinsDown(20, 1);
 
-            for (int i = 0; i < 20; i++)
-            {
-                game.Roll(1);
-            }
-
-            Assert.AreEqual(20, game.Score());
+            Assert.AreEqual(20, _game.Score());
         }
+
+        [Test]
+        public void Test_game_player_rolls_one_spare()
+        {
+            RollSpare();
+            _game.Roll(3);
+            RollByMultipleAndPinsDown(17, 0);
+
+            Assert.AreEqual(16, _game.Score());
+        }
+
+        [Test]
+        public void Test_game_player_rolls_one_strike()
+        {
+            RollStrike();
+            _game.Roll(3);
+            _game.Roll(4);
+            RollByMultipleAndPinsDown(16, 0);
+
+            Assert.AreEqual(24, _game.Score());
+        }
+
+        [Test]
+        public void Test_game_player_plays_perfect_300_game()
+        {
+            RollByMultipleAndPinsDown(12, 10);
+
+            Assert.AreEqual(300, _game.Score());
+        }
+
+        [Test]
+        public void Test_game_player_trys_to_roll_to_many_balls()
+        {
+            var ex = Assert.Throws<IndexOutOfRangeException>(() => RollByMultipleAndPinsDown(22, 1));
+            Assert.That(ex.Message, Is.EqualTo("Index was outside the bounds of the array."));
+        }
+
+        #endregion
+
+        #region Fixture Helper/Shared Methods
+
+        private void RollByMultipleAndPinsDown(int rolls, int pinsDown)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                _game.Roll(pinsDown);
+            }
+        }
+
+        private void RollSpare()
+        {
+            _game.Roll(5);
+            _game.Roll(5);
+        }
+
+        private void RollStrike()
+        {
+            _game.Roll(10);
+        }
+
+        #endregion
     }
 }
--- BowlingGame/Game.cs
+++ BowlingGame/Game.cs
@@ -1,17 +1,87 @@
-﻿namespace BowlingGame
+﻿using System;
+
+namespace BowlingGame
 {
     public class Game
     {
-        private int score;
+        #region Private Variables
 
-        public void Roll(int v)
+        private readonly int[] _rolls = new int[21];
+        private int _currentRoll;
+
+        public void Roll(int pinsDown)
         {
-
+            try
+            {
+                _rolls[_currentRoll++] = pinsDown;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
+
+        #endregion
+
+        #region Instance Methods
 
         public int Score()
         {
+            int score = 0;
+            int roll = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if(FrameIsStrike(roll))
+                {
+                    score += GetScoreForStrikeFrame(roll);
+                    roll++;
+                }
+                else if (FrameIsSpare(roll))
+                {
+                    score += GetScoreForSpareFrame(roll);
+                    roll += 2;
+                }
+                else
+                {
+                    score += GetScoreForRegularFrame(roll);
+                    roll += 2;
+                }
+            }
+
             return score;
         }
+
+        #endregion
+
+        #region Instance Helper Methods
+
+        private bool FrameIsSpare(int roll)
+        {
+            return _rolls[roll] + _rolls[roll + 1] == 10;
+        }
+
+        private bool FrameIsStrike(int roll)
+        {
+            return _rolls[roll] == 10;
+        }
+
+        private int GetScoreForRegularFrame(int roll)
+        {
+            return _rolls[roll] + _rolls[roll + 1];
+        }
+
+        private int GetScoreForSpareFrame(int roll)
+        {
+            return 10 + _rolls[roll + 2];
+        }
+
+        private int GetScoreForStrikeFrame(int roll)
+        {
+            return 10 + _rolls[roll + 1] + _rolls[roll + 2];
+        }
+
+        #endregion
     }
 }
